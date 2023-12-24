74e8927ac0531998259027f816fda5d98335921b
Nwigwe Chidubem <krain9421@gmail.com>
2023-12-24 18:38:45 +0000

    commit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,9 +1,54 @@
 #!/usr/bin/python3
-"""Module containing a script that reads stdin and computes metrics"""
+"""This module contains a script that reads stdin and computes metrics
+    Input format:
+    `<IP Address> -
+    [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>`
+    (if the format is not this one, the line must be skipped)
+"""
 import random
 import sys
-# from time import sleep
-# import datetime
+from time import sleep
+import datetime
+
+
+def checker(line):
+    """
+        Function that checks if a line is of a specific format
+    """
+    st_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    checknum = 0
+    if (len(line[0].split(".")) == 4):
+        # print("CHECK 0: PASSED")
+        checknum = checknum + 1
+    if (line[1] == "-"):
+        # print("CHECK 1: PASSED")
+        checknum = checknum + 1
+    if (line[2]):
+        # print("CHECK 2: PASSED")
+        checknum = checknum + 1
+    if (line[3]):
+        # print("CHECK 3: PASSED")
+        checknum = checknum + 1
+    if (line[4] == "\"GET"):
+        # print("CHECK 4: PASSED")
+        checknum = checknum + 1
+    if (line[5] == "/projects/260"):
+        # print("CHECK 5: PASSED")
+        checknum = checknum + 1
+    if (line[6] == "HTTP/1.1\""):
+        # print("CHECK 6: PASSED")
+        checknum = checknum + 1
+    if (line[7] in st_codes):
+        # print("CHECK 7: PASSED")
+        checknum = checknum + 1
+    if (int(line[8]) in range(1, 1024)):
+        # print("CHECK 8: PASSED")
+        checknum = checknum + 1
+
+    if (checknum == 9):
+        return True
+    else:
+        return False
 
 
 line = []
@@ -13,9 +58,12 @@ file_size = 0
 while (True):
     for i in range(10):
         line = sys.stdin.readline().split()
-        file_size = file_size + int(line[8])
-        st_dict[line[7]] = st_dict[line[7]] + 1
-        # print(line)
+        if (checker(line)):
+            file_size = file_size + int(line[8])
+            st_dict[line[7]] = st_dict[line[7]] + 1
+            # print(line)
+        else:
+            pass
     print("File size: {}".format(file_size))
     for k in st_dict:
         print("{}: {}".format(k, st_dict[k]))
