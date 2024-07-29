8103aa213593a6b1b40c8cc4306db1cccf8d3715
Nwigwe Chidubem <krain9421@gmail.com>
2024-07-29 12:22:11 +0100

    Made some changes

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -22,26 +22,45 @@ def validUTF8(data):
     for byte in data:
         if pass_byte > 0:
             pass_byte -= 1
+            i = i + 1
             pass
         elif not over_bytes:
             if (byte & 0x80) == 0:
+                # LOGGING
+                # print("1 BYTE UTF-8")
                 pass
             elif (byte & 0xe0) == 0xc0:
                 over_bytes = 1
+                # LOGGING
+                # print("2 BYTE UTF-8")
             elif (byte & 0xf0) == 0xe0:
                 over_bytes = 2
+                # LOGGING
+                # print("3 BYTE UTF-8")
             elif (byte & 0xf8) == 0xf0:
                 over_bytes = 3
+                # LOGGING
+                # print("4 BYTE UTF-8")
             else:
+                # LOGGING
+                # print("INVALID UTF-8")
                 return False
         else:
             end = i + over_bytes
-            if end >= len(data):
+            if end > len(data):
                 return False
             for byt in data[i:end]:
+                # LOGGING
+                # print(data[i:end])
                 if (byt & 0xc0) != 0x80:
+                    # LOGGING
+                    # print("INVALID UTF-8")
                     return False
             pass_byte = over_bytes
             over_bytes = 0
-            i = i + 1  # Increment the index after every iteration
+        i = i + 1  # Increment the index after every iteration
+
+        # Check if dataset is shorter than over_bytes
+        if (over_bytes and i + over_bytes > len(data)):
+            return False
     return True
