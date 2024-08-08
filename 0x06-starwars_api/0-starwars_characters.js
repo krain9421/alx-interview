3af1bbfb06ca655b8f650e20078487a546e79e67
Nwigwe Chidubem <krain9421@gmail.com>
2024-08-08 23:19:49 +0100

    Commit

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+
+function fetchCharacters(movieId) {
+  request.get(`https://swapi-api.alx-tools.com/api/films/${movieId}/`, { json: true }, async (err, res, body) => {
+    if (err) console.log(err);
+    for (const character of body.characters) {
+      request.get(character, { json: true }, (error, res, body) => {
+        if (error) console.log(error);
+        console.log(body.name);
+      });
+      await new Promise((resolve) => setTimeout(resolve, 200));
+    }
+  });
+}
+
+const movieId = process.argv[2];
+if (!movieId) {
+    console.error('Please provide a movie ID (e.g., 3 for "Return of the Jedi").');
+} else {
+    fetchCharacters(movieId);
+}
