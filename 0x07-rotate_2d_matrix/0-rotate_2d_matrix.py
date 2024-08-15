484fe58ab793e8d56f22eaf5d492f7251daf7fdb
Nwigwe Chidubem <krain9421@gmail.com>
2024-08-15 21:53:59 +0100

    Added files

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module for implementing rotation of a 2D Matrix
+"""
+import copy
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Given an n x n 2D matrix, rotate it 90 degrees clockwise.
+
+    Matrix must be edited in-place.
+    Matrix will have 2 dimensions and will not be empty.
+    """
+    # Create a deep copy of the matrix to be used in the loop
+    matrix_tmp = copy.deepcopy(matrix)
+
+    for i in range(len(matrix_tmp)):
+        transposed_row = []
+        for row in matrix_tmp:
+            transposed_row.append(row[i])
+        transposed_row.reverse()
+        matrix[i] = transposed_row
+
+    # Clear the matrix copy
+    matrix_tmp.clear()
